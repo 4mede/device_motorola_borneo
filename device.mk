187d2d61cdade103ed5a90f34d80161f45bc4c52
TheScarastic <warabhishek@gmail.com>
2022-09-23 16:05:58 +0530

    borneo: Introduce biometric 2.3 hal
     * wrap 2.1 hal and add additional function
    
    Change-Id: I4a66760fb8ca88de4e9229b7aec90052470ce6f0

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.borneo
 
 # Init
 PRODUCT_PACKAGES += \
