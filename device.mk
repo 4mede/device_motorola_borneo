182315f3c0c3bf984787ea0f50cc0ea9fa05518d
Tommaso Fonda <fondatommaso2@gmail.com>
2023-07-22 08:58:40 +0200

    rhode: overlay: Add Updater overlay
    This specifies endpoints for the stable and beta release channels, and
    the custom changelog URL.
    
    Change-Id: Icf6bf289f71e1d6fc576dc4774e2b52ce892aaa3

--- device.mk
+++ device.mk
@@ -33,7 +33,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 29
