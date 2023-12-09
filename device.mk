6d5ee43a81cf9f7c9c037c70d4261914fa3dfa77
MD Raza <delightreza@gmail.com>
2023-12-09 20:53:19 +0530

    borneo: Add touchscreen firmware & modules to recovery
    Fixes touch in recovery
    
    Signed-off-by: MD Raza <delightreza@gmail.com>
    Change-Id: I098134ab95d953a61e08493adb045d1d9f05cfa4

--- device.mk
+++ device.mk
@@ -27,7 +27,8 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.oem.fingerprint.sh \
     init.oem.fingerprint2.sh \
-    init.mmi.overlay.rc
+    init.mmi.overlay.rc \
+    init.recovery.touch.rc
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
