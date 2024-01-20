1009d8caebccdd9b1e34519289bc275e7b9ac30b
electimon <electimon@gmail.com>
2024-01-20 02:42:49 +0000

    borneo: Arrow bringup
    Change-Id: I3f0386b5660253d82f621984aae4cdcb4746e384

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_borneo.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_borneo-user \
-    lineage_borneo-userdebug \
-    lineage_borneo-eng
+    $(LOCAL_DIR)/arrow_borneo.mk
