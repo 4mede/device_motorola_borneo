af6b1a8737245e2f0a017d5b3d2026d84d2b93f6
TogoFire <togofire@mailfence.com>
2024-03-02 10:51:56 -0500

    borneo: introduce Viper4Android FX
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: sabrina010209 <mcpe11640@gmail.com>

--- halcyon_borneo.mk
+++ halcyon_borneo.mk
@@ -10,9 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/borneo/device.mk)
 
+<<<<<<< HEAD:halcyon_borneo.mk
 # Inherit some common Halcyon Project stuff.
 $(call inherit-product, vendor/halcyon/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit from ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := halcyon_borneo
