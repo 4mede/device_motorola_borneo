b42d9400cf07857cdcf8a6b3c87e91d4dfdffca6
4mede <4mede@tutamail.com>
2024-12-15 12:37:35 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,8 +18,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1600
-TARGET_SCREEN_WIDTH := 720
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Init
 PRODUCT_PACKAGES += \
