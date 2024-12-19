b673f20a23a4f65ea653b55fe8a85b1437d44b9c
4mede <4mede@tutamail.com>
2024-12-18 21:52:11 -0500

    Update yaap_borneo.mk

--- yaap_borneo.mk
+++ yaap_borneo.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/borneo/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_borneo
