59d5e1d6dc809a87a2ff9e06a359ccbb68368a99
chrisl7 <wandersonrodriguesf1@gmail.com>
2025-03-21 09:10:13 -0400

    borneo: aospa bringup part 2

--- device.mk
+++ device.mk
@@ -17,6 +17,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+android.hardware.biometrics.fingerprint@2.1.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
