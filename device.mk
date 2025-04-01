899d171c17c3ba1d199f6fd1ce8a7033d098980e
4mede <4mede@tutamail.com>
2025-04-01 11:54:24 -0400

    borneo: Don't build MotoCamera for now
    Video Recording is broken in QPR2

--- device.mk
+++ device.mk
@@ -29,10 +29,10 @@ PRODUCT_PACKAGES += \
     init.recovery.touch.rc
 
 # Moto Camera 3
-TARGET_MOTCAMERA3 := borneo
-TARGET_USES_MOTCAMERA3 := true
+#TARGET_MOTCAMERA3 := borneo
+#TARGET_USES_MOTCAMERA3 := true
 
-$(call inherit-product, vendor/motorola/MotCamera3/motcamera3.mk)
+#$(call inherit-product, vendor/motorola/MotCamera3/motcamera3.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
