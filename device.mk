ed4a9dad9cf3c38f63621a6e85a4c708bfc974e4
4mede <4mede@tutamail.com>
2025-04-11 07:47:57 -0400

    borneo: Fix building overlays
    Change-Id: I550f8e9ef3a5e990136266669fe1bd4dfaf5cb26

--- device.mk
+++ device.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-custom
 
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 29
