71cfcd51f62126138fcc503a6d39cce2f72fd48e
4mede <4mede@tutamail.com>
2025-06-11 07:09:40 -0400

    borneo: genesis bringup
    Change-Id: I35fe181388a9a58d6c0bff546c600314fe00900e

--- genesis_borneo.mk
+++ genesis_borneo.mk
@@ -10,11 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/borneo/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Genesis stuff.Add commentMore actions
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_borneo
+PRODUCT_NAME := genesis_borneo
 PRODUCT_DEVICE := borneo
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g power (2021)
