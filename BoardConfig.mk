bdba2c59294632b9b686a40a1f4b62a06e63e9a2
4mede <4mede@tutamail.com>
2025-10-22 21:09:28 -0400

    borneo: Decommonize arch flags and optimize for Kryo 260

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,19 @@ include device/motorola/sm6225-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/borneo
     
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT_RUNTIME := kryo260
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT_RUNTIME := kryo260
+
 # Board Info
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
